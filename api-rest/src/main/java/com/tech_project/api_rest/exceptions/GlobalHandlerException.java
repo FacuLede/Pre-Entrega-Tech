7df1913f42bd428d0be5e70c29572f3a843eaf52
Facundo <facundo_lede@hotmail.com>
2025-12-04 14:40:31 -0300

    add: agregué el menejo de excepciones de la base de datos.

--- api-rest/src/main/java/com/tech_project/api_rest/exceptions/GlobalHandlerException.java
+++ api-rest/src/main/java/com/tech_project/api_rest/exceptions/GlobalHandlerException.java
@@ -3,6 +3,7 @@ package com.tech_project.api_rest.exceptions;
 import java.time.Instant;
 import java.util.stream.Collectors;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.validation.FieldError;
@@ -20,5 +21,13 @@ public class GlobalHandlerException {
         p.setProperty("errors", ex.getBindingResult().getFieldErrors().stream()
             .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage)));
         return p;
-}
+    }   
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ProblemDetail handleDataIntegrity(DataIntegrityViolationException ex) {
+        ProblemDetail p = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        p.setTitle("Data Integrity Violation");
+        p.setDetail("A database constraint was violated");
+        return p;
+    }
 }
